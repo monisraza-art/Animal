62c84cf23e3d97038df35e4a13889bbd33c2b538
Monis <monisraza444@gmail.com>
2025-05-29 00:15:17 +0500

    h

---
+++ src/app/api/auth/[...nextauth/route.ts
@@ -0,0 +1,19 @@
+// import {signIn} from "next-auth/react"
+// import GoogleProvider from "next-auth/providers/google"
+
+// const authOptions = {
+//     providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID as string,
+//       clientSecret: process.env.GOOGLE_SECRET as string,
+//     })
+// ],
+// database: process.env.DATABASE_URL,
+// secret: process.env.NEXTAUTH_SECRET
+// callbacks: {
+//      async signIn({profile}: {
+//         profile: {email: string, name : string, picture : string}
+//      })
+//      async session()
+// }
+// }
